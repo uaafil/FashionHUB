53f6c6ce339f4e885f7232b6e4faeb9f1fd4fad7
uaafil <102312487+uaafil@users.noreply.github.com>
2025-06-16 13:10:52 +0530

    Delete ecommerce directory

--- ecommerce/src/main/java/com/example/ecommerce/dto/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String email;
-    private String password;
-}
--- ecommerce/src/main/java/com/example/ecommerce/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.repository;
-
-import com.example.ecommerce.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByUserMail(String userMail);
-}
